1873e5f20a2dee327eb50a9f0bd56a422c4ecc1b
drd-2rcl <drd.rclrd@gmail.com>
2020-06-03 21:33:32 -0300

    Create initial endpoint to list and create values

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,33 @@
-import express from "express";
+import express, { response } from "express";
 
 const app = express();
+
+app.use(express.json());
+const users = ["Diogo", "Leandro", "Carlos"];
 
 app.get("/users", (req, res) => {
   console.log("Listagem de usuários");
 
-  res.json(["Diogo", "Leandro", "Carlos"]);
+  res.json(users);
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  const user = users[id];
+
+  res.json(user);
+});
+
+app.post("/users", (req, res) => {
+  const data = req.body;
+
+  const user = {
+    name: data.name,
+    email: data.email,
+  };
+
+  return res.json(user);
 });
 
 app.listen(3333);
