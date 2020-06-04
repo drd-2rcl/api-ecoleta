b491728c643b09f507db12516b2b9b0308de96dc
drd-2rcl <drd.rclrd@gmail.com>
2020-06-03 23:19:53 -0300

    Clean file

--- src/server.ts
+++ src/server.ts
@@ -1,33 +1,11 @@
 import express, { response } from "express";
 
+import routes from "./routes";
+
 const app = express();
 
 app.use(express.json());
-const users = ["Diogo", "Leandro", "Carlos"];
 
-app.get("/users", (req, res) => {
-  console.log("Listagem de usuários");
-
-  res.json(users);
-});
-
-app.get("/users/:id", (req, res) => {
-  const id = Number(req.params.id);
-
-  const user = users[id];
-
-  res.json(user);
-});
-
-app.post("/users", (req, res) => {
-  const data = req.body;
-
-  const user = {
-    name: data.name,
-    email: data.email,
-  };
-
-  return res.json(user);
-});
+app.use(routes);
 
 app.listen(3333);
