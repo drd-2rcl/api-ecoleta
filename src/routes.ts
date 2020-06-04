bbf1f75cae10709ed3532d1528f845ebb931372a
drd-2rcl <drd.rclrd@gmail.com>
2020-06-04 08:46:00 -0300

    Add list items and create points

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,59 @@
 import express, { response } from "express";
+import knex from "./database/connection";
 
 const routes = express.Router();
 
-routes.get("/", (req, res) => {
-  return res.json({ message: "Hi" });
+routes.get("/items", async (req, res) => {
+  const items = await knex("items").select("*");
+
+  const serializedItems = items.map((item) => {
+    return {
+      id: item.id,
+      title: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`,
+    };
+  });
+  return res.json(serializedItems);
+});
+
+routes.post("/points", async (req, res) => {
+  const {
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    items,
+  } = req.body;
+
+  // transaction
+  const trx = await knex.transaction();
+
+  const insertedIds = await trx("points").insert({
+    image: "image-fake",
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+  });
+
+  const point_id = insertedIds[0];
+
+  const pointItems = items.map((item_id: number) => {
+    return {
+      item_id,
+      point_id,
+    };
+  });
+
+  await trx("point_items").insert(pointItems);
+
+  return res.json({ success: true });
 });
 
 export default routes;
