febb2ce943be5a84cc922b9ec135c91847072314
drd-2rcl <drd.rclrd@gmail.com>
2020-06-04 10:42:55 -0300

    Created method show point

--- src/controller/PointsController.ts
+++ src/controller/PointsController.ts
@@ -2,6 +2,22 @@ import { Request, Response } from "express";
 import knex from "../database/connection";
 
 class PointsController {
+  async show(req: Request, res: Response) {
+    const { id } = req.params;
+
+    const point = await knex("points").where("id", id).first();
+
+    if (!point) {
+      return res.status(400).json({ message: "Point not found" });
+    }
+
+    const items = await knex("items")
+      .join("point_items", "items.id", "=", "point_items.item_id")
+      .where("point_items.point_id", id);
+
+    return res.json({ point, items });
+  }
+
   async create(req: Request, res: Response) {
     const {
       name,
