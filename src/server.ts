149b3e71221bd61dfe58ad9367cb6325b0b00998
drd-2rcl <drd.rclrd@gmail.com>
2020-06-04 08:46:29 -0300

    Add route to upload images

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import express, { response } from "express";
-
+import path from "path";
 import routes from "./routes";
 
 const app = express();
 
 app.use(express.json());
-
 app.use(routes);
+
+app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
 
 app.listen(3333);
